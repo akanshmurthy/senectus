a54a671b2dd4ad29e05bf126722487518169dfb6
Akansh Murthy <ak45m16@gmail.com>
2016-09-25 13:32:33 -0700

    add key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     url = URI.parse('https://qa-api.humana.com/hackathon/members/0872257542951/profiles/clinical')
     req = Net::HTTP::Get.new(url.to_s)
     req['Accept'] = 'application/json'
-    req.add_field('x-humana-api-key', ENV["HUMANA_KEY"])
+    req.add_field('x-humana-api-key', "Fv9ehdvat58PbOh2PX2ePGEMxRWqaMIu")
     res = Net::HTTP.new(url.host, url.port)
     res.use_ssl = true
     res.verify_mode = OpenSSL::SSL::VERIFY_NONE
